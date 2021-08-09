90121b38f78d961d4cb822e6ea7eacc899a762c4
sichan <sichan1301@naver.com>
2021-08-09 23:41:08 +0900

    add login component & login css

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,11 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APPFIREBASE_DB_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-}
-firebase.initializeApp(firebaseConfig);
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
