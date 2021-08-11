2b5cbe2c197e37543c9e4d63aefc901c0d2aa777
sichan <sichan1301@naver.com>
2021-08-11 16:11:15 +0900

    add editor and preview layout css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = ({onLogout}) =>(
+const Header = ({ onLogout }) =>(
     <header className={styles.header}>
         {onLogout && (<button className={styles.logout} onClick = {onLogout}>Logout</button>)}
         <img className={styles.logo} src="/images/logo.png" alt="logo" />
