78726a717726f4b361b4eb21c6e9cbc8a88fd22d
sichan <sichan1301@naver.com>
2021-08-16 17:42:04 +0900

    add loading spinner

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -10,8 +10,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
   const emailRef = useRef();
   const messageRef = useRef();
 
-  const { name, company, title, email, message, theme, fileName, fileURL } =
-    card;
+  const { name, company, title, email, message, theme, fileName } = card;
 
   const onFileChange = (file) => {
     updateCard({
@@ -89,7 +88,7 @@ const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
         onChange={onChange}
       />
       <div className={styles.fileInput}>
-        <FileInput name={name} onFileChange={onFileChange} />
+        <FileInput name={fileName} onFileChange={onFileChange} />
       </div>
       <Button name="Delete" onClick={onSubmit} />
     </form>
